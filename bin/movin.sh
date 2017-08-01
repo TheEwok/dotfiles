3b3f21dde5c19f2e6b1f36f6608553e6f28079f2
Baz O'Mahony <bomahony@shutterstock.com>
2017-08-01 13:36:48 -0600

    Colors

--- bin/movin.sh
+++ bin/movin.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 export PATH=/usr/local/bin:$PATH
 cat <<- EOF >> $HOME/.bash_profile
+# Set CLICOLOR if you want Ansi Colors in iTerm2 
+export CLICOLOR=1
+
+# Set colors to match iTerm2 Terminal Colors
+export TERM=xterm-256color
+
 if [ -f ~/.bashrc ]; then
    source ~/.bashrc
 fi
