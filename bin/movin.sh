91f92ca3cc525a681ba3f38092a5cada20d25939
Baz O'Mahony <the.ewok@gmail.com>
2017-08-01 10:35:58 -0600

    Update movin.sh

--- bin/movin.sh
+++ bin/movin.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+export PATH=/usr/local/bin:$PATH
+
+# Install Brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew doctor
+brew update
+
+# Brew recipes
+brew install git
 
 # Get teh dotfiles
 git clone git@github.com:TheEwok/dotfiles.git
@@ -13,3 +22,5 @@ else
   make movein;
 fi
 
+echo "****"
+echo "Don't forget to source .bashrc"
